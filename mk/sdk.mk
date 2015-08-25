28e9d31d8b9fc2c602157dc35a64e8b1d8d98693
Kevin Kiningham <kkiningh@samsara.com>
2015-08-24 19:05:38 -0700

    WIP: changed auto download feature to be easier to use

--- mk/sdk.mk
+++ mk/sdk.mk
@@ -8,6 +8,8 @@ endif
 
 # SDK path defaults to the lib directory
 SDK_PATH ?= lib/nRF51_SDK_v$(SDK_VERSION)
+SDK_ZIP  := $(SDK_PATH).zip
+SDK_URL  := https://developer.nordicsemi.com/nRF51_SDK/nRF51_SDK_v9.x.x/nRF51_SDK_9.0.0_2e23562.zip
 
 # Add required startup files
 SDK_SRC  += $(SDK_PATH)/components/toolchain/gcc/gcc_startup_nrf51.s
@@ -23,13 +25,13 @@ SDK_INC  += $(SDK_PATH)/components/toolchain/gcc
 CPPFLAGS += -DNRF51       # Required for nrf.h
 
 # Download the SDK if we don't already have it
-#$(SDK_PATH) $(SDK_SRC): $(SDK_ZIP)
-#	@echo "Installing SDK v$(SDK_VERSION)"
-#	$(Q)scripts/nrf51-sdk.sh $(SDK_VERSION) $(SDK_PATH)
-##	unzip preserves the timestamps from the zip file
-## 	Use touch to convince make we don't need to unzip after the first time
-#	$(Q)find $(SDK_PATH) -print0 | xargs -0 touch -r $(SDK_PATH)
-#
+$(SDK_ZIP):
+	$(Q)curl $(SDK_URL) --progress-bar -o "$@"
+
+$(addprefix $(SDK_PATH)/,$(shell cat "mk/sdk/$(SDK_VERSION)")): $(SDK_ZIP)
+	$(Q)unzip -qq -o -d $(SDK_PATH) $(SDK_ZIP)
+	$(Q)find $(SDK_PATH) -print0 | xargs -0 touch -r $(SDK_PATH)
+
 # Add the SDK files we need to the list of files to compile
 SRC     += $(SDK_SRC)
 SYS_INC += $(SDK_INC)
